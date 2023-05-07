0463597330139b4e687009699da7ea862f0af070
MichelC <a@a.a>
2023-05-07 08:10:53 +0200

    commit test

--- migrations/Version20230507060422.php
+++ migrations/Version20230507060422.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230505080751 extends AbstractMigration
+final class Version20230507060422 extends AbstractMigration
 {
     public function getDescription(): string
     {
